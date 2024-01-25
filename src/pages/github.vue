72b6fc32cfe71f17a85998c9879ff77949bd8bcb
Anar <aanar1k14@gmail.com>
2024-01-25 13:42:30 +0200

    updates

--- src/pages/github.vue
+++ src/pages/github.vue
@@ -1,6 +1,30 @@
 <script setup lang="ts">
-
 import UiPageTitle from '~/components/Ui/UiPageTitle.vue';
+
+const route = useRoute();
+const router = useRouter();
+
+interface IQueryRoute {
+  page?: string
+}
+
+const currentPage = computed<number>({
+  get () {
+    const { page }: IQueryRoute = route.query;
+
+    const newPage = Number(page);
+
+    return !isNaN(newPage) ? newPage : 1;
+  },
+
+  set (newValue: number) {
+    router.push({
+      query: {
+        page: newValue
+      }
+    });
+  }
+});
 </script>
 
 <template>
@@ -8,9 +32,13 @@ import UiPageTitle from '~/components/Ui/UiPageTitle.vue';
     <ui-page-title>
       github
     </ui-page-title>
+
+    <UPagination
+      v-model="currentPage"
+      :page-count="5"
+      :total="100"
+      :prev-button="{ icon: 'i-heroicons-arrow-small-left-20-solid', label: 'Prev', color: 'gray' }"
+      :next-button="{ icon: 'i-heroicons-arrow-small-right-20-solid', trailing: true, label: 'Next', color: 'gray' }"
+    />
   </div>
 </template>
-
-<style scoped>
-
-</style>
