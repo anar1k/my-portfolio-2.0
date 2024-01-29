a9f204115531f6f7d146829f5e8b9b577ad1a07a
Anar <aanar1k14@gmail.com>
2024-01-29 22:36:55 +0200

    updates

--- src/components/Ui/UiCardGradient.vue
+++ src/components/Ui/UiCardGradient.vue
@@ -9,16 +9,16 @@
       base: 'relative z-[1] before:absolute before:z-[-1] before:inset-[1px] before:rounded-[inherit]',
       background: `
         bg-gradient-to-br
-        from-[#404040FF]
-        to-[#40404000]
+        from-primary-50
+        dark:from-[#404040FF]
+        dark:to-[#40404000]
         to-50%
 
         before:bg-gradient-to-br
-        before:bg-[#202022FF]
-        before:from-[#2D2D2E40]
-        before:to-[#1B1B1D00]
+        dark:before:bg-[#202022FF]
+        dark:before:from-[#2D2D2E40]
+        dark:before:to-[#1B1B1D00]
       `,
-      shadow: 'shadow-2xl',
       ring: ''
     }"
   >
